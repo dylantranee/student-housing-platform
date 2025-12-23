de3893f800e7fc62da22ddecf5f6bf5fb86ad078
Van Tran <tavan.iuyouth@gmail.com>
2025-12-23 18:59:05 +0700

    Fix linting dependencies and migrate ESLint config (#6)

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'tailwind.config.mjs', 'postcss.config.mjs', '.build']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
